e3700e3c22dc79aaa33681641c26d93d0715b7f3
Sanchez17140 <sanchez17140@gmail.com>
2025-05-01 02:38:53 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
 import "bootstrap";
 import "./style.css";
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
+window.onload = function()
+ {
+  let element=document.querySelector(".excuse")
+  console.log("Any string");
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let end = ['.com'];
   console.log("Hello Rigo from the console!");
+  const getRanInt=(array)=>{
+    return Math.floor(Math.random() * array.length);
+  }
+  element.innerHTML= pronoun [getRanInt(pronoun)] +adj[getRanInt(adj)] +noun[getRanInt(noun)] +end[getRanInt(end)];
+
+  const button = document.getElementById("btn btn-primary");
+  button.addEventListener('click', () =>{
+      alert('Button was clicked!');
+  })
 };
